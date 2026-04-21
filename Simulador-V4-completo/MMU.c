86777d8a6759be6f48fe65afd146cc9b7d5d0104
Luucky25 <lucas.alvaper@gmail.com>
2026-04-21 19:09:18 +0200

    [*] (organize) - Adjuntar una nueva carpeta que mantenga todos los cambios y deshacer los cambios hasta el ejercicio 4, este incluido

---
+++ Simulador-V4-completo/MMU.c
@@ -0,0 +1,94 @@
+#include "MMU.h"
+#include "Buses.h"
+#include "Processor.h"
+#include "Simulator.h"
+
+// The base register
+int registerBase_MMU;
+
+// The limit register
+int registerLimit_MMU;
+
+// The MAR register
+int registerMAR_MMU;
+
+// The CTRL register
+int registerCTRL_MMU;
+
+void MMU_SetCTRL (int ctrl) {
+	int firstInvalidAddress;
+	int logicalAddress = registerMAR_MMU;
+	int isAddressValid = 0;
+
+	registerCTRL_MMU=ctrl&0x3;
+
+	if (Processor_PSW_BitState(EXECUTION_MODE_BIT)) {
+		firstInvalidAddress=MAINMEMORYSIZE; // Protected mode
+		if (logicalAddress >= 0 && logicalAddress < firstInvalidAddress) isAddressValid = 1;
+	} else {
+		firstInvalidAddress=registerLimit_MMU; // Non-protected mode
+		if (logicalAddress >= 0 && logicalAddress < firstInvalidAddress) {
+			isAddressValid = 1;
+			registerMAR_MMU+=registerBase_MMU;
+		}
+	}
+
+	switch (registerCTRL_MMU) {
+		case CTRLREAD:
+		case CTRLWRITE:
+			if (isAddressValid) {
+				// Send to the main memory HW the physical address to write in
+				Buses_write_AddressBus_From_To(MMU, MAINMEMORY);
+				// Tell the main memory HW to read
+				// registerCTRL_MMU is CTRLREAD or CTRLWRITE 
+				Buses_write_ControlBus_From_To(MMU,MAINMEMORY);
+				// Success
+			 	registerCTRL_MMU |= CTRL_SUCCESS;
+			} else {
+				// Fail
+				registerCTRL_MMU |= CTRL_FAIL;
+				Processor_RaiseException(INVALIDADDRESS);
+			}
+  			break;
+  		default:
+				registerCTRL_MMU |= CTRL_FAIL;
+				break;
+  	}
+  	// registerCTRL_MMU return value was CTRL_SUCCESS or CTRL_FAIL
+  	Buses_write_ControlBus_From_To(MMU,CPU);
+}
+
+// Getter for registerCTRL_MMU
+int MMU_GetCTRL () {
+  return registerCTRL_MMU;
+}
+
+// Setter for registerMAR_MMU
+void MMU_SetMAR (int newMAR) {
+  registerMAR_MMU = newMAR;
+}
+
+// Getter for registerMAR_MMU
+int MMU_GetMAR () {
+  return registerMAR_MMU;
+}
+
+// Setter for registerBase_MMU
+void MMU_SetBase (int newBase) {
+  registerBase_MMU = newBase;
+}
+
+// Getter for registerBase_MMU
+int MMU_GetBase () {
+  return registerBase_MMU;
+}
+
+// Setter for registerLimit_MMU
+void MMU_SetLimit (int newLimit) {
+  registerLimit_MMU = newLimit;
+}
+
+// Getter for registerLimit_MMU
+int MMU_GetLimit () {
+  return registerLimit_MMU;
+}
